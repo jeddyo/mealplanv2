3e9a8b313feb0bd1da13cc9da31cdb270b36f99e
unknown <johngarcia.edward@gmail.com>
2025-03-26 10:01:32 -0700

    fix bug

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,12 +3,14 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { copyFileSync } from 'fs';
 
-// ⬇ Custom plugin to copy _redirects into dist
 function copyRedirects() {
   return {
     name: 'copy-redirects',
     closeBundle() {
-      copyFileSync(resolve(__dirname, '_redirects'), resolve(__dirname, 'dist/_redirects'));
+      copyFileSync(
+        resolve(__dirname, '_redirects'),
+        resolve(__dirname, 'dist/_redirects')
+      );
     },
   };
 }
