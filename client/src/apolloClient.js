f1129f781db5772ab0c01e2a9bb631a5e02f3975
unknown <johngarcia.edward@gmail.com>
2025-03-25 20:54:24 -0700

    render deployment

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000/graphql", // Your GraphQL server
+  uri: "https://meal-plan-7bfa.onrender.com/graphql", // Your GraphQL server
 });
 
 const authLink = setContext((_, { headers }) => {
